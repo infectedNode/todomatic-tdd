b187327a4100a411856e9ea50305ba9a1e9f18cb
Shivam Sharma <shivam231198@gmail.com>
2022-02-01 00:37:33 +0530

    successfull rendering test

--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render( <App />, document.getElementById('root'));
+const DATA = [
+    { id: "todo-0", name: "Eat", completed: true },
+    { id: "todo-1", name: "Sleep", completed: false },
+    { id: "todo-2", name: "Code", completed: false }
+];
+  
+
+ReactDOM.render(<App tasks={DATA} />, document.getElementById("root"));
